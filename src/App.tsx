37b699baaa1982d5c64b2d750fcd0d10669f0898
AlejandroDeveloper2 <90485793+AlejandroDeveloper2@users.noreply.github.com>
2024-05-20 12:50:43 -0500

    Mejora de rendimiento

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import { BrowserRouter } from "react-router-dom";
-import { pdfjs } from "react-pdf";
 
 import { MainRoutes } from "@routes/index";
 
 import "react-toastify/dist/ReactToastify.css";
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.js",
-  import.meta.url
-).toString();
 
 function App() {
   return (
--- src/layout/protected-layout/ProtectedLayout.tsx
+++ src/layout/protected-layout/ProtectedLayout.tsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { pdfjs } from "react-pdf";
 
 import { useSession } from "@modules/auth/hooks";
 
 import { Header, Navigation } from "@modules/core/components";
 
 import styles from "./ProtectedLayout.module.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString();
 
 const ProtectedLayout = (): JSX.Element => {
   useSession(1000);
